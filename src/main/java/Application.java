f82e1c6761c3512a845722c0aadbea591a9294a3
shamshaev <an.shamshaev@gmail.com>
2024-05-24 19:36:49 +0300

    Change application

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,33 +8,30 @@ public class Application {
         // Создаем соединение с базой в памяти
         // База создается прямо во время выполнения этой строчки
         // Здесь hexlet_test — это имя базы данных
-        var conn = DriverManager.getConnection("jdbc:h2:mem:hexlet_test");
+        try (var conn = DriverManager.getConnection("jdbc:h2:mem:hexlet_test")) {
+            var sql = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), phone VARCHAR(255))";
+            // Чтобы выполнить запрос, создадим объект statement
+            try (var statement = conn.createStatement()) {
+                statement.execute(sql);
+            }
 
-        var sql = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), phone VARCHAR(255))";
-        // Чтобы выполнить запрос, создадим объект statement
-        try (var statement = conn.createStatement()) {
-            statement.execute(sql);
-        }
+            var sql2 = "INSERT INTO users (username, phone) VALUES"
+                    + " ('tommy', '123456789'), ('mary', '987667892'), ('andrew', '876356431')";
+            try (var statement2 = conn.createStatement()) {
+                statement2.executeUpdate(sql2);
+            }
 
-        var sql2 = "INSERT INTO users (username, phone) VALUES"
-            + " ('tommy', '123456789'), ('mary', '987667892'), ('andrew', '876356431')";
-        try (var statement2 = conn.createStatement()) {
-            statement2.executeUpdate(sql2);
-        }
-
-        var sql3 = "SELECT * FROM users";
-        try (var statement3 = conn.createStatement()) {
-            // Здесь вы видите указатель на набор данных в памяти СУБД
-            var resultSet = statement3.executeQuery(sql3);
-            // Набор данных — это итератор
-            // Мы перемещаемся по нему с помощью next() и каждый раз получаем новые значения
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString("username"));
-                System.out.println(resultSet.getString("phone"));
+            var sql3 = "SELECT * FROM users";
+            try (var statement3 = conn.createStatement()) {
+                // Здесь вы видите указатель на набор данных в памяти СУБД
+                var resultSet = statement3.executeQuery(sql3);
+                // Набор данных — это итератор
+                // Мы перемещаемся по нему с помощью next() и каждый раз получаем новые значения
+                while (resultSet.next()) {
+                    System.out.println(resultSet.getString("username"));
+                    System.out.println(resultSet.getString("phone"));
+                }
             }
         }
-
-        // Закрываем соединение
-        conn.close();
     }
 }
