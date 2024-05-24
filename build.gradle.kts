596af044ea8ac2b7bdc7d782786a0d6e20610ed7
shamshaev <an.shamshaev@gmail.com>
2024-05-24 18:36:43 +0300

    Add Application

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("java")
+    application
+}
+
+application {
+    mainClass = "Application"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.h2database:h2:2.2.220")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
